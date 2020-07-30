223366d1c802e92b6f33594a73a94afa4454e18e
Darren Dube <dz03dube@gmail.com>
2020-07-30 13:58:21 +0200

    update

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -21,7 +21,19 @@ export default () => (
           className="primary-content" 
           dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
         />
-        <Link to='/contact' className="button -primary">Contact Me &rarr;</Link>
+
+        <form
+  action="https://buttondown.email/api/emails/embed-subscribe/darrendube"
+  method="post"
+  target="popupwindow"
+  onsubmit="window.open('https://buttondown.email/darrendube', 'popupwindow')"
+  class="embeddable-buttondown-form"
+>
+        <div className="button-wrapper">
+        <input type="text" className="text-box"></input>
+        <input type="submit" className="button -primary" value=" Subscribe &rarr;"></input>
+        </div>
+        </form>
       </div>
     )}
   />
