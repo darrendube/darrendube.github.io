87112cb267405a8a9e05c69beebd086f51f92854
Darren Dube <dz03dube@gmail.com>
2020-08-02 12:37:26 +0200

    added fonts

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,6 +4,9 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
+    <Link to="/404">Home</Link>
+    <Link to="/404">Projects</Link>
+   
     <Link to="/contact">Contact</Link>
     <ThemeChanger/>
   </nav>
