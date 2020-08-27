81e69c9750650c3ea2bdcd256adc70f9a8a17cdb
darrendube <45855781+darrendube@users.noreply.github.com>
2020-08-27 11:08:50 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Delog GatbsyJS Starter`,
-        short_name: `Delog`,
+        name: `Darren Dube`,
+        short_name: `Darren Dube`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#381696`,
