acb52c6dee565156bcf0340a5da921eddcab28bf
Darren Dube <dz03dube@gmail.com>
2020-08-28 17:50:14 +0200

    added sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,6 +63,7 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
