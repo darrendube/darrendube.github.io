6224c25cb521cdfa8e0baef5cdbb26d988bd8a8f
Darren Dube <dz03dube@gmail.com>
2021-03-23 19:33:55 +0200

    error fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -123,7 +123,7 @@ module.exports = {
     },
     `gatsby-plugin-sass`, 
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-netlify-cms`,
+   
     'gatsby-plugin-dark-mode',
     // siteURL is a must for sitemap generation
     `gatsby-plugin-sitemap`,
