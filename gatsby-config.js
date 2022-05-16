dd0b564ce8f4b5ec784e086a8673f57b906d93ff
DD <dz03dube@gmail.com>
2022-05-16 18:59:36 +0200

    updates article

--- gatsby-config.js
+++ gatsby-config.js
@@ -181,7 +181,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it. replace with yours
-        trackingId: "UA-164743872-1",
+        trackingId: "G-PPRCK7RBP3",
         head: true,
       },
     },
